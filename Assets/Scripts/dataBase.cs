40297a299e22cf999f3850e26072c91b7388199b
Steven Cortez <steven_cortez@msn.com>
2017-04-11 06:53:16 -0500

    alpha pre release 0.9

--- Assets/Scripts/dataBase.cs
+++ Assets/Scripts/dataBase.cs
@@ -7,25 +7,28 @@ using Mono.Security.Cryptography;
 using UnityEngine.UI;
 
 public class dataBase : MonoBehaviour {
+    public GameObject[] newPlayerInfo;
+    public GameObject[] highScoreText;
     public class Player
     {
-        private string fName,lName,gender,email,password;
-        private int score,id;
+        private string fName, lName, gender;
+        private double score;
+        private int id;
 
-        public Player(string fName,string lName,int score,string email,string password,int ID)
+        public Player(string f, string l, string g, double s, int i)
         {
-            this.fName = fName;
-            this.lName = lName;
-            this.score = score;
-            this.email = email;
-            this.password = password;
-            this.id = ID;
+            this.fName = f;
+            this.lName = l;
+            this.gender = g;
+            this.score = s;
+            this.id = i;
         }
-        public string getFname()
+
+        public string getFirstName()
         {
             return this.fName;
         }
-        public string getLname()
+        public string getLastName()
         {
             return this.lName;
         }
@@ -33,85 +36,132 @@ public class dataBase : MonoBehaviour {
         {
             return this.gender;
         }
-        public string getEmail()
-        {
-            return this.email;
-        }
-        public string getPass()
-        {
-            return this.password;
-        }
-        public int getScore()
-        {
-            return this.score;
-        }
         public int getId()
         {
             return this.id;
         }
+        public double getScore()
+        {
+            return this.score;
+        }
 
     }
-    public List<Player> playerList = new List<Player>();
-
-    void Start()
-    {
-        loadDataBase();
+    private List<Player> playerList = new List<Player>();
+  
+    void Update()
+    {   
        
-        
-        //SaveDataBase();
-    }
 
-    public void loadDataBase()
-    {
-        StreamReader dataBaseIn = new StreamReader("Assets\\Database.dat");
-        string myLine;
-
-        while ((myLine = dataBaseIn.ReadLine()) != null)
-            playerList.Add(new global::dataBase.Player(myLine.Split(':')[0], myLine.Split(':')[1], int.Parse(myLine.Split(':')[2]), myLine.Split(':')[3], myLine.Split(':')[4], int.Parse(myLine.Split(':')[5])));
-        dataBaseIn.Close();
-        playerList = SortedData();
     }
-    public void newPlayer(string fName, string lName, int score, string email, string password, int ID)
+    public void setHighScoresVisible(bool value)
     {
-        playerList.Add(new global::dataBase.Player(fName, lName, score, email, password, ID));
-        playerList = SortedData();
-    }
-    public void SaveDataBase()
-    {
-        StreamWriter outFile = new StreamWriter("Assets\\Database.dat");
+        highScoreText[0].GetComponent<Text>().text = "Names";
+        highScoreText[1].GetComponent<Text>().text = "Scores";
         for (int k=0;k<playerList.Count;k++)
         {
-            outFile.Write(playerList[k].getFname() + ":" + playerList[k].getLname() + ":" + playerList[k].getScore() + ":" + playerList[k].getEmail() +":" + playerList[k].getPass() + ":" + playerList[k].getId());
-            outFile.WriteLine();
+            highScoreText[0].GetComponent<Text>().text += "\n" + playerList[k].getFirstName() + " " + playerList[k].getLastName();
+            highScoreText[1].GetComponent<Text>().text += "\n" + playerList[k].getScore();
+        }
+    }
+    void Start()
+    {
+
+        
+        loadDatabase();
+        playerList = sortDatabase();
+
+       // newPlayer("Steven3", "c", "Male");
+        saveDatabase();
+
+    }
+    //void newPlayer(string f, string l, string g)
+    public void newPlayer()
+    {
+        string f = newPlayerInfo[0].GetComponent<Text>().text;
+        string l = newPlayerInfo[1].GetComponent<Text>().text;
+        string g = "";
+        if (newPlayerInfo[2].GetComponent<Toggle>().isOn)
+        {
+            g = "Male";
+            newPlayerInfo[3].GetComponent<Toggle>().isOn = false;
+        }
+        else if (newPlayerInfo[3].GetComponent<Toggle>().isOn)
+        {
+            g = "Female";
+            newPlayerInfo[2].GetComponent<Toggle>().isOn = false;
+        }
+
+        if (playerList.Count > 0)
+        {
+            for (int k = 0; k < playerList.Count; k++)
+            {
+                if (f.Equals(playerList[k].getFirstName()) && l.Equals(playerList[k].getLastName()))
+                    return;
+            }
+        }
+            List<int> idList = new List<int>();
+            for (int k = 0; k < playerList.Count; k++)
+            {
+                idList.Add(playerList[k].getId());
+            }
+            idList.Sort();
+            playerList.Add(new Player(f, l, g, 0f, idList[idList.Count - 1] + 1));
+            saveDatabase();
+        
+    }
+    List<Player> sortDatabase()
+    {
+        List<Player> returnList = new List<Player>();
+        List<double> dList = new List<double>();
+        for (int k=0;k<playerList.Count;k++)
+        {
+            dList.Add(playerList[k].getScore());
+        }
+
+        dList.Sort();
+        dList.Reverse();
+        for (int k=0;k<dList.Count;k++)
+        {
+            for (int i=0;i<playerList.Count;i++)
+            {
+                if (playerList[i].getScore() == dList[k])
+                    returnList.Add(playerList[i]);
+            }
+        }
+
+        return returnList;
+    }
+    void loadDatabase()
+    {
+        StreamReader inFile = new StreamReader("Assets\\Database.dat");
+
+        string myLine = "";
+        while ((myLine = inFile.ReadLine())!=null)
+        {
+            playerList.Add(new global::dataBase.Player(myLine.Split(':')[0], myLine.Split(':')[1], myLine.Split(':')[2], double.Parse(myLine.Split(':')[3]), int.Parse(myLine.Split(':')[4])));
+        }
+        inFile.Close();
+    }
+    void saveDatabase()
+    {
+        StreamWriter outFile = new StreamWriter("Assets\\Database.dat");
+
+        for (int k=0;k<playerList.Count;k++)
+        {
+            outFile.WriteLine(playerList[k].getFirstName() + ":" + playerList[k].getLastName() + ":" +
+                playerList[k].getGender() + ":" + playerList[k].getScore() + ":" + playerList[k].getId());
+
+           
         }
         outFile.Close();
     }
-    public List<Player> SortedData()
-    {
-        List<Player> list = new List<Player>();
-        List<int> myList = new List<int>();
-
-        for (int k=0;k<playerList.Count-1;k++)
-        {
-            myList.Add(playerList[k].getScore());
-            
-        }
-        myList.Sort();
-        myList.Reverse();
-        for (int k=0;k<myList.Count;k++)
-        {
-            for (int i =0;i<playerList.Count;i++)
-            {
-                if (playerList[i].getScore() == myList[k])
-                    list.Add(playerList[i]);
-            }
-        }
-        return list;
-    }
     void OnGUI()
     {
-      //  for (int k = 0; k < playerList.Count; k++)
-        //    GUI.Label(new Rect(10, 10 + (k * 24), 2566, 24), playerList[k].getFname() +" :: " + playerList[k].getScore());
+        //if (showHighScores)
+        {
+            //for (int k = 0; k < playerList.Count; k++)
+                //GUI.Label(new Rect(500, 10 + (k * 24), 2566, 24), playerList[k].getFirstName() + " :: " + playerList[k].getScore());
+        }
     }
 
     
